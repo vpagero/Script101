eb58e939cd8d15d92115f0846fe6f85a03ebcea1
vpagero <78521124+vpagero@users.noreply.github.com>
2022-02-04 02:51:56 +0100

    Update beginners_script.sh

--- beginners_script.sh
+++ beginners_script.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
+#! /bin/bash
+#1. The above indicates its a script (bash script to be presized)
 
+
+#2. Comments
+#To write a comment, one must start first with a # on the line of the comment.just like this line.
+
+#3. Echo
+echo "Hello World!"
+#This prints out Hello World after tghe script is executed!
+
+#4. 
